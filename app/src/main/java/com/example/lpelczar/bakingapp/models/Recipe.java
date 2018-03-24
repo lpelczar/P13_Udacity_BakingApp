e60386decd9994ebe0f0f1e335a061c77cf2f814
lpelczar <lukepelczar@gmail.com>
2018-03-24 17:40:27 +0100

    Add recipe model

---
+++ app/src/main/java/com/example/lpelczar/bakingapp/models/Recipe.java
@@ -0,0 +1,50 @@
+package com.example.lpelczar.bakingapp.models;
+
+
+
+import java.util.List;
+
+public class Recipe {
+
+    private int id;
+    private String name;
+    private int servings;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " Name: " + name;
+    }
+}
