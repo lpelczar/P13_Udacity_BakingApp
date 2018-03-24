90a2c2581ba9db1bec11d5c0827d180dd46cd668
lpelczar <lukepelczar@gmail.com>
2018-03-24 18:32:56 +0100

    Add ingredients and recipes lists to recipe model

--- app/src/main/java/com/example/lpelczar/bakingapp/models/Recipe.java
+++ app/src/main/java/com/example/lpelczar/bakingapp/models/Recipe.java
@@ -1,7 +1,6 @@
 package com.example.lpelczar.bakingapp.models;
 
 
-
 import java.util.List;
 
 public class Recipe {
@@ -10,41 +9,12 @@ public class Recipe {
     private String name;
     private int servings;
     private String image;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getServings() {
-        return servings;
-    }
-
-    public void setServings(int servings) {
-        this.servings = servings;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
+    private List<Ingredient> ingredients;
+    private List<RecipeStep> steps;
 
     @Override
     public String toString() {
-        return "ID: " + id + " Name: " + name;
+        return "ID: " + id + " Name: " + name + "\n Ingredients: " + ingredients +
+                "\n Steps: " + steps;
     }
 }
