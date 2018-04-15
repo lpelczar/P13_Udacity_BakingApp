6f2a0f0a4b21fe870358af3aa52fb583ec4fef75
lpelczar <lukepelczar@gmail.com>
2018-04-15 12:22:51 +0200

    Implement parcelable in ingredient model

--- app/src/main/java/com/example/lpelczar/bakingapp/models/Ingredient.java
+++ app/src/main/java/com/example/lpelczar/bakingapp/models/Ingredient.java
@@ -1,9 +1,12 @@
 package com.example.lpelczar.bakingapp.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 
-public class Ingredient implements RecipeDetail {
+public class Ingredient implements RecipeDetail, Parcelable {
 
     private double quantity;
     private String measure;
@@ -25,5 +28,34 @@ public class Ingredient implements RecipeDetail {
     @Override
     public String toString() {
         return "Quantity: " + quantity + " Measure: " + measure + " Name: " + name;
+    }
+
+    //Parcelable
+    public static final Parcelable.Creator<Ingredient> CREATOR = new Parcelable.Creator<Ingredient>() {
+        public Ingredient createFromParcel(Parcel in) {
+            return new Ingredient(in);
+        }
+
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
+
+    private Ingredient(Parcel in){
+        this.quantity = in.readDouble();
+        this.measure = in.readString();
+        this.name =  in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDouble(this.quantity);
+        dest.writeString(this.measure);
+        dest.writeString(this.name);
     }
 }
